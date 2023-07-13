68170837baad1196dc1a875029e8a9cfb313f19f
WinVeggi <dbemperok13@mail.ru>
2023-07-13 09:30:22 +0300

    Задание по пятому уроку готово

--- dz/urls.py
+++ dz/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='main-page'),
+     path('', index, name='main-page'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', advertisement_post, name='advertisement-post'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('profile/', profile, name='profile')
 ]
